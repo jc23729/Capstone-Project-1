17c93aff9c792beb033e2db225c17260e8f08204
Juan Cruz <jc23729@gmail.com>
2021-03-11 15:47:35 -0500

    update

--- data_manager.py
+++ data_manager.py
@@ -1,8 +1,12 @@
 import requests
 
+SHEETY_PRICES_ENDPOINT = 'https://api.sheety.co/ff1183adec43520d59ee2ddd3b4c7588/flightDeals/prices'
+
 
 
 
 class DataManager:
     #This class is responsible for talking to Google Sheets
     pass
+
+print(SHEETY_PRICES_ENDPOINT)
