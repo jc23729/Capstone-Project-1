2e1ff836fc41fd1c6d4954fbd2b5b2e2d70286dc
Juan Cruz <jc23729@gmail.com>
2021-03-11 16:16:28 -0500

    update import pprint

--- data_manager.py
+++ data_manager.py
@@ -1,4 +1,5 @@
 import requests
+import pprint
 
 SHEETY_PRICES_ENDPOINT = 'https://api.sheety.co/ff1183adec43520d59ee2ddd3b4c7588/copyOfFlightDeals/prices'
 
@@ -9,7 +10,12 @@ class DataManager:
 
     def __init__(self):
         self.destination_data = {}
-    #This class is responsible for talking to Google Sheets
-    pass
 
-# print('SHEETY_PRICES_ENDPOINT')
+    def destination_data(self):
+        #step 2 getting the Sheety API to GET all the data in that sheet and print it out
+        response = requests.get(SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data ["prices"]
+        #step 3 Importing pretty print and printing data out again using pprint() to see if it formatted
+        #pprint(data)
+        return self.destination_data
